58214b9f734b1e941797a1eacf15996344e484a0
Srikar Yallala <32783094+srikaryallala@users.noreply.github.com>
2020-01-17 16:53:38 -0600

    Update quantiacsAlgorithm.py

--- quantiacsAlgorithm.py
+++ quantiacsAlgorithm.py
@@ -1,20 +1,22 @@
 import numpy
 
 def myTradingSystem(DATE, OPEN, HIGH, LOW, CLOSE, VOL, OI, P, R, RINFO, exposure, equity, settings):
-    ''' This system uses trend following techniques to allocate capital into the desired equities'''
 
     nMarkets=CLOSE.shape[1]
 
-    periodLong=200 #%[200:10:300]#
-    periodShort=10 #%[20:10:100]#
+    periodLong=200
+    periodShort=10
 
+    #Calculates Simple Moving Average for Short and Long periods
     smaLong=numpy.nansum(CLOSE[-periodLong:,:],axis=0)/periodLong
     smaRecent=numpy.nansum(CLOSE[-periodShort:,:],axis=0)/periodShort
 
+    #Goes long if short-term SMA is greater than long-term and short if vice-versa
     longEquity= numpy.array(smaRecent > smaLong)
     shortEquity= ~longEquity
 
     pos=numpy.zeros((1,nMarkets))
+    #Places positions
     pos[0,longEquity]=1
     pos[0,shortEquity]=0.5
 
@@ -24,10 +26,7 @@ def myTradingSystem(DATE, OPEN, HIGH, LOW, CLOSE, VOL, OI, P, R, RINFO, exposure
 
 
 def mySettings():
-    ''' Define your trading system settings here '''
-
     settings= {}
-
 
     # Futures Contracts
 
@@ -44,8 +43,6 @@ def mySettings():
 		 'WIN', 'XLNX', 'YHOO', 'XRX']
     settings['endInSample'] = '20191224'
     settings['beginInSample'] = '20100101'
-    # settings['beginInSample'] = '20120506'
-    # settings['endInSample'] = '20150506'
     settings['lookback'] = 504
     settings['budget'] = 1000000
     settings['slippage'] = 0.05
